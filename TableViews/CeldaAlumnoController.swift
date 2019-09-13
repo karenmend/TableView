fbd63f684b93a054b96e28a998b878ba8a393ac2
Alumno <ulsa@iMacD109.local>
2019-09-13 14:32:42 -0700

    primer commit

---
+++ TableViews/CeldaAlumnoController.swift
@@ -0,0 +1,19 @@
+//
+//  CeldaAlumnoController.swift
+//  TableViews
+//
+//  Created by Alumno on 9/13/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CeldaAlumnoController: UITableViewCell{
+    
+    
+    
+    
+}
+
+
+
---
+++ TableViews/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Alumno on 9/13/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableView, UITableViewDataSource{
+
+    let alumnos = ["Gabriel", "Ana Lourdes", "Joel", "Jose", "Araceli", "Karen"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    //Contenido de cada celda
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno")
+        
+        return celda!
+    }
+}
+
